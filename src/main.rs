bfca06b869084887e80c25710e6f2271762aa69a
Robert Rusch <rusch@mit.edu>
2016-12-23 10:14:13 -0500

    Messed with the tile implementation some

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,35 @@ extern crate ncurses;
 
 use ncurses::*;
 
+struct Tile {
+    material: Material,
+    temp: f32,
+}
+
+struct Sceen {
+    tiles: &[Tile],
+    x_dim: i16,
+    y_dim: i16,
+}
+
+enum Material {
+    Wood,
+    Metal,
+    Air,
+}
+
+fn print_screen(slice: &[Tile]) {
+    print!
+}
+
+fn
+
 fn main()
 {
-  /* Setup ncurses. */
-  initscr();
-  raw();
+    init_scr();
 
-  /* Allow for extended keyboard (like F1). */
-  keypad(stdscr(), true);
-  noecho();
-
-  /* Invisible cursor. */
-  curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
-
-
-  endwin();
+    let tile = Tile { material: Material::Wood, temp: 70.0 };
+    print_tile(tile);
 }
+
 
